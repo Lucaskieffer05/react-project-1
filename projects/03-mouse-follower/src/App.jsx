4446ee5d32f0570c0262237a6905907efc6f3230
Lucaskieffer05 <lucaskieffer05@gmail.com>
2025-01-22 01:47:25 -0300

    mouse follower finish

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,8 +1,55 @@
+import { useState, useEffect } from "react"
 
-function App() {
+
+const FollowMause = () => {
+
+  const [enabled, setEnable] = useState(false)
+
+  const [position, setPosition] = useState({x: 0, y: 0})
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const {clientX, clientY} = event
+      setPosition({x: clientX, y: clientY})
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
 
   return (
-    
+    <>
+      <div style = {{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}/>
+
+      <button onClick={() => setEnable(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguidor de ratón
+      </button>
+    </>
+  )
+
+}
+
+function App() {
+  return (
+    <main>
+      <FollowMause />
+    </main>
   )
 }
 
